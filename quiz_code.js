2763aa7d21f460cdfe02e085dd981ae1b3a00a3b
kevin-on-davis <kevin.on.davis@gmail.com>
2019-10-07 23:29:44 -0400

    Day 1-Splash page, login and registration pages (in progress)

---
+++ quiz_code.js
@@ -0,0 +1,27 @@
+var btn_examiner = document.querySelector("#examiner");
+var btn_examinee = document.querySelector("#examinee");
+var btn_newReg = document.querySelector("#reg_new_xmnr");
+var xmnr_card = document.querySelector("#new_examiner");
+
+debugger;
+btn_examiner.addEventListener("click", function() 
+{
+    document.querySelector("#sign_in_examiner").style="display:block";
+    document.querySelector("#welcome_page").style="display:none";
+    xmnr_card.style="display:none";
+    event.stopPropagation;
+});
+
+// btn_examinee.addEventListener("click", function() 
+// {
+//     document.getElementById("xmnr_card").style="display:none";
+//     document.getElementById("sign_in_examiner").style="display:block";
+// });
+
+btn_newReg.addEventListener("click", function()
+{
+    document.querySelector("#welcome_page").style="display:none";
+    document.querySelector("#sign_in_examiner").style="display:none";
+    xmnr_card.style="display:block";
+    event.stopPropagation;
+});
