7632c0120684b5572eab671d3f35b4f69d7c0868
kevin-on-davis <kevin.on.davis@gmail.com>
2019-10-10 23:05:02 -0400

    Backup of evening progress.-Oct 10,2019

--- quiz_code.js
+++ quiz_code.js
@@ -7,6 +7,7 @@ var btn_registerThenLogin = $("#new_registration");
 var btn_write_question = $("#save_question");
 var btn_save_quiz = $("#stop_entry");
 var btn_start_quiz = $("#start_quiz");
+var btn_confirm_answer = $("#submit_answer")
 
 var quiz = new Array();
 
@@ -18,6 +19,16 @@ var login_page = $("#sign_in_examiner");
 var question_entry_page = $("#build_quiz");
 var student_quiz_page = $("#student_quiz");
 var quiz_question_pages = $("#quiz_question");
+
+// Constant declaration
+const penalty = 10;
+
+// Variable declaration
+var quiz_life  = 0;
+var question_life = 0;
+var q_index = 0;
+var correct_cnt = 0;
+var incorrect_cnt = 0;
 
 var stop_writing = false;
 // var new
@@ -118,19 +129,54 @@ btn_examinee.on('click', function()
 
 btn_start_quiz.on("click", function()
 {
-    debugger;
+    // debugger;
     event.preventDefault();
     student_quiz_page.css("display", "none");
 
+    q_index = 0;
+    correct_cnt = 0;
+    incorrect_cnt = 0;
+
     ls_quiz = JSON.parse(localStorage.getItem("quiz"));
-    console.log(ls_quiz);
 
-    $("#ask_question").html(ls_quiz[0].questiontext);
-    quiz_question_pages.slideDown("slow");
-    quiz_question_pages.css("display", "block");
+    quiz_life  = 1*(30*ls_quiz.length);
+    // $("#quiz_timer").html(String(Math.floor(quiz_life/3600)).padStart(2, 0) + ':' +
+    //                         String(Math.floor((quiz_life/3600)/60)).padStart(2,0) + ':' +
+    //                         String(Math.floor(quiz_life % 60)).padStart(2, 0));
 
+    load_questions(ls_quiz, q_index);
 
-})
+    setInterval(myQuizInterval, 1000);
+});
+
+btn_confirm_answer.on("click", function()
+{
+    if ($("input[name='option']:checked").val() == ls_quiz[q_index].answer)
+    {
+        // increment correct counter
+        correct_cnt++;
+    }
+    else
+    {
+        // Increment incorrect answer
+        // apply time penalty 
+        incorrect_cnt++;
+        quiz_life-= penalty;
+
+    };
+
+    if (q_index <= ls_quiz.length)
+    {
+        q_index ++;
+        load_questions(ls_quiz, q_index);
+    }
+    else
+    {
+        btn_confirm_answer.attr("disabled", true);
+    };
+    question_life = 0;
+});
+
 
 // Functions
 function write_question()
@@ -148,5 +194,54 @@ function write_question()
 
     quiz_hldr = quiz.push(question);
     $("#myQuestions")[0].reset;
-    // console.log(quiz);
 };
+
+function load_questions(ls_quiz, q_index)
+{
+    // debugger;
+    event.preventDefault();
+    question_life = 0;
+
+    student_quiz_page.css("display", "none");
+    quiz_question_pages.css("display", "block");
+
+    quiz_question_pages.slideUp("slow");
+    if (ls_quiz[q_index].audio_link !== "")
+    {
+        // $(`<audio controls><source src="https://freesound.org/people/RICHERlandTV/sounds/216090/" /></audio>`);
+        $(ls_quiz[q_index].audio_link).appendTo("#audio_box");
+    }
+    $("#ask_question").html(ls_quiz[q_index].questiontext);
+    $("#rb_group").empty();
+    for (ch_index=0; ch_index < ls_quiz[q_index].choices.length; ch_index++)
+    {
+        $(`<span><input id="rb_grp_item" type="radio" name="option" value="${ls_quiz[q_index].choices[ch_index]}" />${ls_quiz[q_index].choices[ch_index]}</span>`).appendTo("#rb_group");
+    }
+
+    quiz_question_pages.slideDown("slow");
+    q_index++;
+}
+
+function myQuizInterval()
+    {
+        // debugger;
+        if (quiz_life <= 0)
+        {
+            clearInterval(myQuizInterval);
+            btn_confirm_answer.attr("disabled", true);
+        }
+        else
+        {
+            $("#quiz_timer").html("Quiz Timer : "+ String(Math.floor(quiz_life/3600)).padStart(2, 0) + ':' +
+            String(Math.floor((quiz_life/3600)/60)).padStart(2,0) + ':' +
+            String(Math.floor(quiz_life % 60)).padStart(2, 0));
+
+            $("#question_timer").html("Question Timer : "+ String(Math.floor(question_life/3600)).padStart(2, 0) + ':' +
+            String(Math.floor((question_life/3600)/60)).padStart(2,0) + ':' +
+            String(Math.floor(question_life % 60)).padStart(2, 0));
+        }
+        quiz_life -= 1;
+        question_life += 1;
+    }
+
+
